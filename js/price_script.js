99bb8d795374f7defa9372a5870a64257f6fb5cf
Alexander Kitaev <kitaev@gmail.com>
2014-10-13 20:16:36 +0200

    WIP: pricing page fixes.

--- js/price_script.js
+++ js/price_script.js
@@ -1,21 +1,79 @@
 // Using jQuery safe
 (function($) {
 
+  function submit(formId, buttonId) {
+    var form = document.getElementById(formId);
+    var inputs = form.getElementsByTagName("input");
+    var params = "formId=" + formId.toUpperCase();
+
+    var savedText = $(buttonId).text();
+
+    for (var i = 0; i <inputs.length; i++){
+      if (inputs[i].type == "text") {
+        inputs[i].style.borderColor="";
+        params += "&" + inputs[i].name + "=" + encodeURIComponent(inputs[i].value);
+      } else if (inputs[i].type == "checkbox") {
+        inputs[i].style.borderColor="";
+        params += "&" + inputs[i].name + "=" + encodeURIComponent(inputs[i].checked);
+      }
+    }
+
+    var request = new XMLHttpRequest();
+
+    request.open("POST", "http://svnkit.com/subgit/send.php", true);
+    var updater = function() {
+      if (request.readyState == 4) {
+        if (request.responseText != null && request.responseText.indexOf('ok|') == 0) {
+          $(buttonId).text('Application Received');
+        } else {
+          $(buttonId).text('Error Occurred');
+        }
+        var handler = function(event) {
+          console.log('enabling back');
+          $(buttonId).text(savedText);
+          $(buttonId).removeClass('disabled');
+          $('#' + formId + ' input').unbind('focus', handler);
+        };
+        $('#' + formId + ' input').on('focus', handler);
+      }
+    };
+
+    request.onreadystatechange = updater;
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    $(buttonId).toggleClass('disabled');
+    $(buttonId).text('Processing...');
+    request.send(params);
+
+    return false;
+  }
+
   $(document).ready(function() {
+    var getAKey = function(buttonId, formId) {
+      return function(event) {
+        event.preventDefault();
+        if ($(buttonId).hasClass('disabled')) {
+          return false;
+        }
+        return submit(formId, buttonId);
+      };
+    };
+
+    $('#get_free_key_button').click(getAKey('#get_free_key_button', 'FREE_AUTO_GENERATED'));
+    $('#get_open_source_key_button').click(getAKey('#get_open_source_key_button', 'OPEN_SOURCE'));
 
     var slideWrap = $("#popup_lists");
 
     $("#prices").on("click", "li", function() {
 
       var href1 = $(this).find(".price_text").next('a').attr('href');
-      for ( var i = 0; i<=10; i++ ) {
+      for ( var i = 0; i <= 10; i++ ) {
         slideWrap.find(".popup:last").prependTo(slideWrap);
         if ( $(href1).index() == 0 ) {
           break;
         }
       }
       $("#popup_cover").show(100);
-      slideWrap.find('.popup:first').find('input, textarea').first().focus();
+      //slideWrap.find('.popup:first').find('input, textarea').first().focus();
     });
 
     $("#popup_cover").find(".close").click(function() {
